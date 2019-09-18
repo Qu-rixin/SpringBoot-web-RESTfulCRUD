bc40dc78bf944b12b95c736b9b42d0a39d74e5af
奇热行 <51438722+Qu-rixin@users.noreply.github.com>
2019-09-18 15:43:28 +0800

    Add files via upload

---
+++ self_crud/src/main/java/com/qrx/self_crud/servlet/MyServlet.java
@@ -0,0 +1,21 @@
+package com.qrx.self_crud.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyServlet extends HttpServlet {
+
+    //处理get请求
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("<h1>hello my servlet</h1>");
+    }
+}
